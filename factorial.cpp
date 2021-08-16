25c2db2128748e1c1d69e93722f55ba68a025b19
Yamini Sharma <b19123@students.iitmandi.ac.in>
2021-08-16 10:38:21 +0530

    factorial function for positive numbers only

--- factorial.cpp
+++ factorial.cpp
@@ -1,6 +1,11 @@
 #include "functions.h"
+#include <iostream>
 
 int factorial(int n){
+    if(n<0){
+        std::cout<<"Please re-enter a positive number";
+        return 0;
+    }
     if(n!=1){
 	return(n * factorial(n-1));
     }
