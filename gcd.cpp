8d9297ab03000ce22310f1d4ab66e1d7c2eca79e
simo411 <b19116@students.iitmandi.ac.in>
2021-08-29 12:45:59 +0530

    Adding non recurssive function of gcd to main

---
+++ gcd.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b)
+{
+	if (a == 0)
+		return b;
+	else if (b == 0)
+		return a;
+	while (a != b) {
+		if (a > b)
+			a = a - b;
+		else
+			b = b - a;
+	}
+	return a;
+}
+
+int main()
+{
+	int a ,b;
+    cin>>a;
+    cin>>b;
+	cout << gcd(a, b) << endl;
+	return 0;
+}
