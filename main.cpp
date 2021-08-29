db93014e20eb410410c736b32b69574e6644f817
simo411 <b19116@students.iitmandi.ac.in>
2021-08-29 13:08:49 +0530

    Added line in main to print GCD

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include "functions.h"
 using namespace std;
 int main(){
+    int x = 56, y=98;
+    cout<<"GCD of"<<x<<" and "<<y<<" is "<<gcd(x,y);
     print_hello();
     cout<< endl;
     cout<<"The factorial of 5 is "<< factorial(5)<< endl;
